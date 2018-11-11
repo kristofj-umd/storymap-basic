6dd3ce88e92982a32599f9cdf7cc13296e00bade
Kris Johnson <kristofj@d.umn.edu>
2018-11-11 01:00:03 -0600

    updating application id

--- config/defaults.js
+++ config/defaults.js
@@ -18,8 +18,8 @@
 define({
   //Default configuration settings for the application. This is where you'll define things like a bing maps key,
   //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
-  "appid": "",
-  "webmap": "24e01ef45d40423f95300ad2abc5038a",
+  "appid": "67dcd0a513a549b091dbcfbbb069e6fa",
+  "webmap": "",
   "oauthappid": null, //"AFTKRmv16wj14N3z",
   //Group templates must support a group url parameter. This will contain the id of the group.
   "group": "",
